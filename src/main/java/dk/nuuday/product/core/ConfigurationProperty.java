2220130026ffa0489dc36e5d6396f83722cc3ae2
sayan2106 <sayancse@gmail.com>
2021-06-25 20:30:34 +0200

    Add config file

---
+++ src/main/java/dk/nuuday/product/core/ConfigurationProperty.java
@@ -0,0 +1,7 @@
+package dk.nuuday.product.core;
+
+public class ConfigurationProperty {
+	public static String dataUrl="https://products-json-example.s3.eu-central-1.amazonaws.com/data.json";
+	public static String voiceUrl="https://products-json-example.s3.eu-central-1.amazonaws.com/voice.json";
+	
+}
